0a5479748dd91412f88450901371c971c3832c0d
Skylar-Brockbank <skylar.brockbank@gmail.com>
2022-02-11 12:03:57 -0800

    add bones and rough in App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import KombuchaList from './KombuchaList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      MasterArray: [],
+      MainContent: 'form',
+      TargetIndex: 0
+    };
+  }
+
+  render(){
+    const main = null;
+    if(this.state.MainContent === 'form'){
+      main = <newKombuchaForm 
+        new = {this.handleNewKombucha}
+      />;
+    }else{
+      main = <KombuchaDetail 
+        target = {this.state.TargetIndex}
+        sell = {this.handleDecrement}
+        restock = {this.setQuantity}
+      />;
+    }
+    return (
+      <React.Fragment>
+        <h2>Page Title</h2>
+        <div id='PrimaryDisplayArea'>
+          <KombuchaList 
+            list={this.state.MasterArray}
+            new={this.setMainContent}
+          />
+          {main}
+        </div>
+      </React.Fragment>
+      
+    );
+  }
+
+
+  setQuantity = (index, qty) => {
+    const transit = [...this.state.MasterArray];
+    transit[index].quantity = qty;
+    this.setState({
+      MasterArray: transit
+    });
+  };
+
+  handleDecrement = (index) => {
+    const q = this.state.MasterArray[index]-1;
+    this.setQuantity(index,q);
+  };
+
+  handleNewKombucha = (entry) => {
+    const transit = [...this.state.MasterArray,entry];
+    this.setState({
+      MasterArray: transit
+    });
+  };
+
+  setMainContent = (form, index) => {
+    if(form){
+      this.setState({
+        MainContent: 'form'
+      });
+    }else{
+      this.setState({
+        MainContent: 'detail',
+        TargetIndex: index
+      });
+    }
+  };
 }
+
 
 export default App;
