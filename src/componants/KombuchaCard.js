96cfafc43ae8c344d05db96f6892ee34d5ac341e
Skylar-Brockbank <skylar.brockbank@gmail.com>
2022-02-11 14:58:28 -0800

    make list cards show out of stock conditionally

--- src/componants/KombuchaCard.js
+++ src/componants/KombuchaCard.js
@@ -2,11 +2,17 @@ import React from "react";
 import PropTypes from 'prop-types';
 
 function KombuchaCard(props){
+  let q = null;
+  if(props.target.qty >0){
+    q=props.target.qty;
+  }else{
+    q="Out of Stock";
+  }
   return(
     <div className='card' onClick={()=> props.imethod('detail', props.dex)}>
       {console.log(props.dex)}
       <h3>{props.target.name}</h3>
-      <p>{props.target.qty}</p>
+      <p>{q}</p>
     </div>
   );
 }
